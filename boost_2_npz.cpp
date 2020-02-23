f54a2622ab52438a8f12ad417576128bb660483c
mexuaz <mexuaz@outlook.com>
2020-02-23 01:11:13 -0800

    final edition

---
+++ boost_2_npz.cpp
@@ -0,0 +1,69 @@
+#include"cnpy.h"
+#include<iostream>
+#include <fstream>
+#include <vector>
+#include <filesystem>
+
+#include <boost/serialization/vector.hpp>
+#include <boost/archive/binary_iarchive.hpp>
+#include <boost/archive/binary_oarchive.hpp>
+
+
+int main(int argc, char** argv)
+{
+    if(argc < 2) {
+        std::cerr << "Usage: " << argv[0] << " <file.ser>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    using EdgeCount = uint64_t;
+
+    std::filesystem::path inFile(argv[1]);
+    std::filesystem::path outFile(argv[1]);
+    outFile.replace_extension(".npz");
+
+
+    std::ifstream ifs;
+    ifs.exceptions(~ifs.goodbit);
+    ifs.open(inFile, std::ios_base::in | std::ios_base::binary);
+
+    boost::archive::binary_iarchive bi(ifs);
+
+    std::vector<EdgeCount> src, dst;
+    std::cout << "Reading binary input file: " << inFile.filename().string() << std::endl;
+    auto t0 = std::chrono::high_resolution_clock::now();
+    bi >> src >> dst;
+    auto ts = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - t0);
+    ifs.close();
+    std::cout << "Number of edges: " << src.size() << std::endl;
+    std::cout << "Reading input file took time: " << ts.count() << " msec" << std::endl;
+
+#if(0)
+    cnpy::npy_save("src.npy", src); // save single vector to file
+    cnpy::npy_save("src.npy", &src[0], {src.size()}); // save vector with shape
+
+    //load it into a new array
+    cnpy::NpyArray arr = cnpy::npy_load("arr1.npy");
+    EdgeCount* loaded_data = arr.data<EdgeCount>();
+    
+    //append the same data to file
+    //npy array on file now has shape (Nz+Nz,Ny,Nx)
+    cnpy::npy_save("arr1.npy", &src[0],{src.size()}, "a");
+#endif
+
+    t0 = std::chrono::high_resolution_clock::now();
+    //now write to an npz file
+    cnpy::npz_save(outFile.string(), "src", src, "w"); // "w" overwrites any existing file
+    cnpy::npz_save(outFile.string(), "dst", dst, "a"); // "a" appends to the file we created above
+    ts = std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - t0);
+    std::cout << "Writing output file took time: " << ts.count() << " sec" << std::endl;
+
+#if(0)
+    cnpy::NpyArray src2 = cnpy::npz_load(outFile.string(), "src");
+
+    //load the entire npz file
+    cnpy::npz_t fl_npz = cnpy::npz_load(outFile.string());
+#endif
+
+    return EXIT_SUCCESS;
+}
---
+++ prepare_submit_cpp.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# usage: prepare_submit_cpp.sh uk-2005 soc-LiveJournal1 ...
+
+RPODIR="$HOME/cnpy"
+SRCDIR="$RPODIR"
+BINDIR="$HOME/bin-cnpy"
+DATDIR="$SCRATCH"
+PROGRM="$BINDIR/boost_2_npz"
+DATAST=("twitter-2010" "uk-2005" "soc-LiveJournal1" "arabic-2005" "indochina-2004" "amazon")
+
+if [[ -d "$RPODIR" ]]; then
+    echo "Syncing repository."
+    cd "$RPODIR"
+    git reset --hard
+    git pull
+else
+    cd $HOME
+    git clone git@github.com:mexuaz/cnpy
+fi
+
+module load gcc/9.1.0 tbb/2018_U5 cmake/3.12.3 boost/1.68.0
+
+if [[ -d "$BINDIR" ]]
+then
+    echo "Removing old $BINDIR and creating new one."
+    rm -rf "$BINDIR"
+fi
+
+mkdir "$BINDIR"
+cd "$BINDIR"
+cmake "$SRCDIR"
+make
+
+
+cd "$DATDIR"
+
+
+if [ $# -eq 0 ]; then
+    for arg in ${DATAST[*]}; do
+        $PROGRM "$arg.ser"
+    done
+else
+    for arg in "$@"; do
+        $PROGRM "$arg.ser"
+    done
+fi
+
+
+
+
